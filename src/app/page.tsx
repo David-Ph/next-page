43157a764e7a62b478a0bf08fa8402f8db6657f9
David <davidphang431@gmail.com>
2023-07-06 23:35:11 +0700

    Layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import Header from "./components/layout/Header";
+import Sidebar from "./components/layout/Sidebar";
 
 export default function Home() {
-  return <main className="text-2xl font-bold italic">Hallo</main>;
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <main className="text-2xl font-bold italic">Hallo</main>
+    </>
+  );
 }
